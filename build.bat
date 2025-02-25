60cbe381bb7c9fddf6b733930acec50aa5552090
Christian Bay <christianb@unity3d.com>
2025-02-25 14:46:26 +0100

    Remove net35 support

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
 set BASEDIR=%~dp0
 set BUILDDIR=%BASEDIR%builds
 set LIBDIR=%BUILDDIR%\lib
-set LIBDIR35=%LIBDIR%\net35
 set LIBDIR40=%LIBDIR%\net40
 set MSBUILD="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 set SEVENZIP="C:\Program Files\7-Zip\7z.exe"
@@ -9,7 +8,6 @@ set SEVENZIP="C:\Program Files\7-Zip\7z.exe"
 ::Setup Directories
 mkdir %BUILDDIR%
 mkdir %LIBDIR%
-mkdir %LIBDIR35%
 mkdir %LIBDIR40%
 
 ::Create Version TXT
@@ -24,9 +22,6 @@ git branch >> %BUILDDIR%\version.txt
 @echo | set /p="Commit: " >> %BUILDDIR%\version.txt
 git log -n 1 --pretty=format:%%H >> %BUILDDIR%\version.txt
 
-::Build Cecil for .NET 3.5
-%MSBUILD% "%BASEDIR%\Mono.Cecil.sln" /t:Build /p:Configuration=net_3_5_Release /p:Platform="Any CPU" /p:OutputPath=%LIBDIR35% || goto failure
-call:CleanupBuild %LIBDIR35%
 ::Build Cecil for .NET 4.0
 %MSBUILD% "%BASEDIR%\Mono.Cecil.sln" /t:Build /p:Configuration=net_4_0_Release /p:Platform="Any CPU" /p:OutputPath=%LIBDIR40% || goto failure
 call:CleanupBuild %LIBDIR40%
